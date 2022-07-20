304cf060b4571d61bf1af8225b44339138962295
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-07-20 12:06:06 +0530

    backend done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 import { createError } from "../error.js";
 import User from "../models/User.js";
-
+import Video from "../models/Video.js";
 export const update = async (req, res, next) => {
   if (req.params.id === req.user.id) {
     try {
@@ -42,10 +42,10 @@ export const getUser = async (req, res, next) => {
 };
 export const subscribe = async (req, res, next) => {
   try {
-    await User.findById(req.user.id, {
-      $push: { subscribedUsers: req.params.id },
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { subcribedUsers: req.params.id },
     });
-    await User.findById(req.params.id, {
+    await User.findByIdAndUpdate(req.params.id, {
       $inc: { subscribers: 1 },
     });
     res.status(200).json("subscription successfull");
@@ -55,10 +55,10 @@ export const subscribe = async (req, res, next) => {
 };
 export const unsubscribe = async (req, res, next) => {
   try {
-    await User.findById(req.user.id, {
-      $pull: { subscribedUsers: req.params.id },
+    await User.findByIdAndUpdate(req.user.id, {
+      $pull: { subcribedUsers: req.params.id },
     });
-    await User.findById(req.params.id, {
+    await User.findByIdAndUpdate(req.params.id, {
       $inc: { subscribers: -1 },
     });
     res.status(200).json("unsubscription successfull");
@@ -67,13 +67,29 @@ export const unsubscribe = async (req, res, next) => {
   }
 };
 export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+    res.status(200).json("the video has been liked");
   } catch (err) {
     next(err);
   }
 };
 export const dislike = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id },
+    });
+    res.status(200).json("the video has been disliked");
   } catch (err) {
     next(err);
   }
